2b91e1ee5b7177d57f00df2affc60a1cd50cd95f
AntonioContrerasRomán <antoniocontrerasroman@gmail.com>
2019-01-11 08:45:16 +0100

    Init repository

---
+++ Backend/Project.Series.Backend/Project.Series.Real/ModelConfigurations/TypeSeriesConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Project.Series.Constants;
+using Project.Series.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Series.Real.ModelConfigurations
+{
+    public class TypeSeriesConfiguration : EntityTypeConfiguration<TypeSeries>
+    {
+        public TypeSeriesConfiguration()
+        {
+            ToTable("TypesSeries", DbConstants.SchemaName);
+
+            Property(ts => ts.Description)
+                .HasMaxLength(1200);
+        }
+    }
+}
---
+++ Backend/Project.Series.Backend/Project.Series.Real/UsersDbContextInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Series.Real
+{
+    public class UsersDbContextInitializer : DropCreateDatabaseAlways<UsersDbContext>
+    {
+        protected override void Seed(UsersDbContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
